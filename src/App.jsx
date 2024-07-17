a28a348c81494d79bf1c123ed2f9e8dd6bdb8909
Soham Pati <sohampati2005@gmail.com>
2024-07-17 15:37:10 +0200

    some more changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+
 import "./App.css";
 import BottomNavBar from "./BottomNavBar";
 import MainContent from "./MainContent";
 
 function App() {
 	const [navValue, setNavValue] = useState(0);
+	
 
 	return (
 		<>
