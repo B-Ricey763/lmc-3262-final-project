59f22ddaa06ecc426ceec49025cf3c26db4c0159
B-Ricey763 <bhanna30@gatech.edu>
2024-07-18 10:54:45 +0200

    Maps, notes, and phoots

--- src/GoogleMyMaps.jsx
+++ src/GoogleMyMaps.jsx
@@ -4,13 +4,10 @@ const GoogleMyMaps = () => {
   return (
     <div style={{ height: "calc(100vh - 56px)", overflow: "hidden" }}>
       <iframe
-        src="https://www.google.com/maps/d/embed?mid=1JMzV-3cURIXzubDCXhFr0vWHJV1kHyU&usp=sharing"
+        title="hello"
+        src="https://www.google.com/maps/d/embed?mid=1j6rk2kCP6c-CwUsEJ0vFpHcZxAvLozw&ehbc=2E312F"
         width="100%"
         height="100%"
-        style={{ border: 0 }}
-        allowFullScreen=""
-        loading="lazy"
-        title="Google MyMap"
       />
     </div>
   );
