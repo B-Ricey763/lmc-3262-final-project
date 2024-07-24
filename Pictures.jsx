b3bc1f6809ed70a032c29b27df92dbfe0c4b2edc
B-Ricey763 <bhanna30@gatech.edu>
2024-07-24 22:24:18 +0200

    stuff

--- Pictures.jsx
+++ Pictures.jsx
@@ -7,13 +7,13 @@ function StandardImageList() {
   return (
     <ImageList
       sx={{ width: "100%", height: "100%" }}
-      cols={3}
+      cols={2}
       variant="masonry"
     >
       {itemData.map((item) => (
         <ImageListItem key={item.id}>
           <img src={`${item.img}`} alt={item.title} loading="lazy" />
-          <ImageListItemBar title={"TEst datee"} position="top" />
+          <ImageListItemBar title={item.date} position="top" />
         </ImageListItem>
       ))}
     </ImageList>
@@ -22,34 +22,70 @@ function StandardImageList() {
 
 const itemData = [
   {
+    id: "0",
+    img: "msg1.jpeg",
+    title: "Intro whats app",
+    date: "07/26/24 11:05",
+  },
+  {
+    id: "0.5",
+    img: "msg2.jpeg",
+    title: "cont.",
+    date: "07/26/24 11:06",
+  },
+  {
+    id: "0.7",
+    img: "msg3.jpeg",
+    title: "Saturn hint",
+    date: "07/26/24 11:30",
+  },
+  {
     id: "1",
     img: "image3.jpg",
     title: "Bookstore",
+    date: "07/26/24 1:30",
   },
   {
-    id: "2",
-    img: "image2.jpg",
-    title: "Scrabble",
+    id: "1.5",
+    img: "msg4.jpeg",
+    title: "Scrabble msg hint",
+    date: "07/26/24 11:45",
+  },
+  {
+    id: "2.3",
+    img: "msg5.jpeg",
+    title: "Clock hint",
+    date: "07/26/24 12:00",
   },
   {
     id: "3",
     img: "image5.jpg",
     title: "Drill",
+    date: "07/26/24 3:30",
+  },
+  {
+    id: "2.5",
+    img: "msg6.jpeg",
+    title: "Drill hint",
+    date: "07/26/24 12:26",
   },
   {
     id: "4",
     img: "image1.jpg",
     title: "Drill Plaque",
+    date: "07/26/24 3:30",
   },
   {
     id: "5",
     img: "image7.jpg",
     title: "Restaurant",
+    date: "07/26/24 4:00",
   },
   {
     id: "6",
     img: "image4.jpg",
     title: "Menu",
+    date: "07/26/24 4:00",
   },
 ];
 function Pictures() {
--- src/Notes.js
+++ src/Notes.js
@@ -1,15 +1,30 @@
 export default [
   {
+    id: "note0",
+    name: "Journaling this Morning",
+    password: "",
+    date: "07/26/24 11:00",
+    content: `
+Ok, I just woke up, and I don't remember what happened. All I have are some notes from Felix and 
+what's app messages with Roxanne (but she didn't go out with us last night), the message screenshots are in my photos I think? I think Felix left some messages??
+
+I don't know what's going on.... wait. WAIT. WHERE IS MY WALLET???
+`,
+  },
+  {
     id: "note1",
     name: "NOAH CLICK ME",
     password: "",
-    date: "6/5/2005 at 20:38",
+    date: "07/26/24 1:00",
     content: `
 
-Heeeyyy Felix,
-Last nigt was crazzzzyy yy right?? You dont r ember much, but you got my stuff and I got urs. les play a game, haha. Follow the clues, find me... or maybe ill find u. Who nows?!
+Heeeyyy Noah,
+Last nigt was crazzzzyy yy right?? You dont r ember much, but it wasss.. wild. just a tad mad (rhymme?)
+but alc dos wundful stuf evn whn ur frens a annoy self, ba bad 
+
+les play a game. Follow the clues, find me... or maybe ill find u. Who nows?!
 Start where we ended –  House of Weekend. Look fur where the music never dies, but tech is shiny n' bright.
-Ive left sum  riddles…
+Ive left sum  riddles… maybe ull learn a thing or two from this >:|
 
 BATTERY AT 30% GOTTA MAKE THESE cLUES
 `,
@@ -18,7 +33,10 @@ BATTERY AT 30% GOTTA MAKE THESE cLUES
     id: "note2",
     name: "first CLuez",
     password: "",
+    date: "07/26/24 1:11",
     content: `
+You cant b callin me names, you know?? U know meee, time for riddlin! 
+
 In the place where ringz do shine,
 Where techies flock n gadgets align.
 Count the spheres tha circle right wide,
@@ -31,19 +49,24 @@ BATTERY AT 26% hAVING FUN
     id: "note3",
     name: "puzzling words?",
     password: "5",
+    date: "07/26/24 1:30",
     content: `
-Okay   so you know how much I loooove Scrabble, right? Its my favorite game ever! I left a clue in a place that sells books and games... yeah, thats it! Find the Scrabble game in the b ookstore at Alexa Mall. Look for it near the entrance. Do you remember how much it costs $$? 
+Okay   so you know how much I loooove Scrabble, right? Its my favorite game ever! ya ya ya its nerdy n crap i know you think it s dumb, but i hav e ur monney so uu playy by my rulles!!
+
+I left a clue in a place that sells books and games... yeah, thats it! Find the Scrabble game in the b ookstore at Alexa Mall. Look for it near the entrance. Do you remember how much it costs $$? 
 
 good LUCK, buddyyy! Hope yur having as much fun as i am! 
 
-ATTT 22% uR DOING GOOD
+ATTT 22% uR DOING GOOD 
 `,
   },
   {
     id: "note4",
     name: "Clocking it ooout",
     password: "1495",
+    date: "07/26/24 2:00",
     content: `
+beee mor careful abt whatt u say, especiallllllly to friendsss
 
 Time for a code, at the clock... thingy,
 Where the hours... are wobblyy and spinny.
@@ -52,7 +75,6 @@ circle around, as you shall walk,
 the hours cut between the time
 thus the code that you will find
 
-
 19% STILL GOING STrONG
 `,
   },
@@ -60,6 +82,7 @@ thus the code that you will find
     id: "note5",
     name: "big DRILLA",
     password: "10",
+    date: "07/26/24 3:30",
     content: `
 Look at the metal twista sign..... what’s the date when the workers stopped working?? YOU NEED TO KNOW,. its all there on the sign, it's DRILLED into my head Ha! The year... or maybe the month? ITS SO COOL i have to take a picture and post it!!
 
@@ -70,6 +93,7 @@ LOW POWER MODE IM 12% ON THE cLOCK
     id: "note6",
     name: "idle 3/15",
     password: "1931",
+    date: "07/26/24 4:00",
     content: `
 heyy y….i got hungry. You better know my favrit order here, we used to go alllllllllll the time. les jus say... That it was the price of a major betrayal of a time long past, in ancient the Ancient Roman Empire of old. 
 
@@ -80,16 +104,23 @@ NOW 5% hELP!!
     id: "note7",
     name: "the FINAL countdown",
     password: "1390",
+    date: "07/26/24 4:30",
     content: `
-so u see ive been clever... battery % is special, whats the letters that are lower? shove em together and teh answer reveals, the spot where wwe can finnally heall-- wholly holy 
+so u see ive been clever... battery % is special, whats the letters that are lower? shove em together and teh answer reveals, the spot where wwe can finnally heall-- wholly holy. hope u learnin. patience and kindness... or not,atp imm soo gonne
 `,
   },
   {
     id: "note8",
     name: "THE spot.. a new beginin",
     password: "church",
+    date: "07/26/24 5:00",
     content: `
-This is the end, your wallet is in charity. L bozo 
+This is the ENDDDD!! uu made it.
+
+Time to reflect. I hope you didn't get too frustrated by my puzzles. Here, take a moment. The quiet of the church surrounds you. Do you even remember what you said to me last night? Probably not. Just think about next time you insult a friend; I was really hurt. Probably shouldnttt of stolenn urr wallet...
+
+yeahh uh aboutt that... it felll inta ta collectorrs binn .. okk good nigghhht :)))
+pay ya bak later.. 1 millin beers :)
 `,
   },
 ];
